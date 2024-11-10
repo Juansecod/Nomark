c44782c47f4b129f86050b43ee0754462e16221e
juan.riosva <juan.riosva@campusucc.edu.co>
2024-11-10 03:00:20 +0000

    Create deep link to redirect when finish the payment

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:myapp/pantallas/HomeScreen.dart';
 import 'package:myapp/pantallas/Splash.dart';
 import 'firebase_options.dart';
 
@@ -16,9 +17,54 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Splash(),
+    return MaterialApp(
       title: 'No Mark',
+      initialRoute: '/',
+      onGenerateRoute: (settings) {
+        // Capturar los enlaces profundos
+        if (settings.name != null) {
+          final Uri uri = Uri.parse(settings.name!);
+          print(uri);
+          // Lógica para manejar deep links
+          if (uri.scheme == 'nomarkapp' && uri.host == 'payment') {
+            if (uri.path == '/success') {
+              return MaterialPageRoute(
+                builder: (_) => PaymentSuccessScreen(),
+              );
+            } else if (uri.path == '/failure') {
+              return MaterialPageRoute(
+                builder: (_) => PaymentFailureScreen(),
+              );
+            }
+          }
+        }
+
+        // Ruta por defecto (si no es un deep link)
+        return MaterialPageRoute(
+          builder: (_) => HomeScreen(),
+        );
+      },
+      home: const Splash(),
+    );
+  }
+}
+
+class PaymentSuccessScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Payment Success")),
+      body: Center(child: Text("Payment Successful!")),
+    );
+  }
+}
+
+class PaymentFailureScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Payment Failure")),
+      body: Center(child: Text("Payment Failed!")),
     );
   }
 }
